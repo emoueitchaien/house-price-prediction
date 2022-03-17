da03bf7ee49ae53f5d6e946601059b3f3e36bd6a
Mohan Pradhan <manojpradhan628@gmail.com>
2022-03-18 00:23:53 +0545

    web UI for implementing prediction model

---
+++ flaskPredictionProject/app.py
@@ -0,0 +1,37 @@
+# pip install flask, scikit-learn, pandas, pickle-mixin
+
+from flask import Flask, render_template, request
+import pandas as pd
+import pickle
+import locale
+
+app = Flask(__name__)
+
+data = pd.read_csv("final_cleaned_data.csv")
+pipe = pickle.load(open('ridge_model.pkl', 'rb'))
+
+
+@app.route('/')
+def index():  # put application's code here
+    locations = sorted(data['location'].unique())
+    return render_template('index.html', locations=locations)
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    location = request.form.get('location')
+    bhk = float(request.form.get('bhk'))
+    bath = float(request.form.get('bath'))
+    sqft = request.form.get('total_sqft')
+
+    # print(location, bhk, bath, sqft)
+
+    locale.setlocale(locale.LC_MONETARY, 'en_IN')
+    inputs = pd.DataFrame([[location, sqft, bath, bhk]], columns=['location', 'total_sqft', 'bath', 'bhk'])
+    prediction = str(locale.currency(round(pipe.predict(inputs)[0] * 1e5, 2), grouping=True)).replace("₹ ", "")
+
+    return prediction
+
+
+if __name__ == '__main__':
+    app.run()
